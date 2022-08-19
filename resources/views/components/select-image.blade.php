cbb05d52a45a79f7123425df4191dc1da3f0339e
Matsuda1129 <69665638+Matsuda1129@users.noreply.github.com>
2022-08-19 10:00:47 +0900

    product create

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -11,10 +11,13 @@ $modal = 'modal-3';
 if ( $name=== 'image4') {
 $modal = 'modal-4';
 }
+if ( $name=== 'image5') {
+$modal = 'modal-5';
+}
 @endphp
 
 <div class="modal micromodal-slide" id="{{ $modal }}" aria-hidden="true">
-    <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+    <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
         <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{ $modal }}-title">
             <header class="modal__header">
                 <h2 class="text-x1 text-gray-700" id="{{ $modal }}-title">
@@ -28,6 +31,7 @@ $modal = 'modal-4';
                     <div class="w-1/4 p-2 mb:p-4">
                         <div class="border rounded-md p-2 mb:p-4">
                             <img class="image" data-id="{{ $name }}_{{ $image->id }}" data-file="{{ $image->filename }}"
+                                {{-- data-id2="{{ $iamge->id}}" --}} {{-- data-name="{{ $name}}" --}}
                                 data-path="{{ asset('storage/products/') }}" data-modal="{{ $modal }}"
                                 src="{{ asset('storage/products/' . $image->filename)}}">
                             <div class="text-gray-700">
@@ -46,9 +50,8 @@ $modal = 'modal-4';
     </div>
 </div>
 
-
 <div class="flex justify-around items-center mb-4">
-    <a data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
+    <a class="py-2 py-4 bg-gray-200" data-micromodal-trigger="{{ $modal }}" href='javascript:;'>ファイルを選択</a>
     <div class="w-1/4">
         <img id="{{ $name }}_thumbnail" src="">
     </div>
--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -15,7 +15,59 @@
                         <div class="-m-2">
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
-                                    <select name="category">
+                                    <label for="name" class="leading-7 text-sm text-gray-600">商品名 *必須</label>
+                                    <input type="text" id="name" name="name" value="{{ old('name')}}" required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="information" class="leading-7 text-sm text-gray-600">商品情報 *必須</label>
+                                    <textarea type="text" id="information" name="information" required rows="10"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{ old('information')}}</textarea>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="price" class="leading-7 text-sm text-gray-600">価格 *必須</label>
+                                    <input type="number" id="price" name="price" value="{{ old('price')}}" required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="sort_order" class="leading-7 text-sm text-gray-600">表示順</label>
+                                    <input type="number" id="sort_order" name="sort_order"
+                                        value="{{ old('sort_order')}}"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="quantity" class="leading-7 text-sm text-gray-600">初期在庫 *必須</label>
+                                    <input type="number" id="quantity" name="quantity" value="{{ old('quantity')}}"
+                                        required
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="shop_id" class="leading-7 text-sm text-gray-600">販売する店舗</label>
+                                    <select name="shop_id" id="shop_id"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                        @foreach ($shops as $shop)
+                                        <option value="{{ $shop->id}}">
+                                            {{$shop->name}}
+                                        </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative">
+                                    <label for="category" class="leading-7 text-sm text-gray-600">カテゴリー</label>
+                                    <select name="category" id="category"
+                                        class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                         @foreach($categories as $category)
                                         <optgroup label="{{ $category->name }}">
                                             @foreach($category->secondary as $secondary)
@@ -31,6 +83,17 @@
                             <x-select-image :images="$images" name="image2" />
                             <x-select-image :images="$images" name="image3" />
                             <x-select-image :images="$images" name="image4" />
+                            <x-select-image :images="$images" name="image5" />
+                            <div class="p-2 w-1/2 mx-auto">
+                                <div class="relative flex justify-around">
+                                    <div>
+                                        <input class="mr-2" type="radio" name="is_selling" value="1" checked>販売中
+                                    </div>
+                                    <div>
+                                        <input class="mr-2" type="radio" name="is_selling" value="0">停止中
+                                    </div>
+                                </div>
+                            </div>
                             <div class="p-2 w-full flex justify-around mt-4 ">
                                 <button type="button" onclick="location.href='{{route('owner.products.index')}}'"
                                     class=" text-black bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
@@ -51,9 +114,12 @@
  image.addEventListener('click', function(e){ 
  const imageName = e.target.dataset.id.substr(0, 6) 
  const imageId = e.target.dataset.id.replace(imageName + '_', '') 
+// const imageName = e.target.dataset.name
+// const imageId = e.target.dataset.id2
  const imageFile = e.target.dataset.file
  const imagePath = e.target.dataset.path
  const modal = e.target.dataset.modal
+ console.log(imageName, imageId, imageFile, imagePath, modal)
  document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
  document.getElementById(imageName + '_hidden').value = imageId
  MicroModal.close(modal); 
