4397d363a5593643b1ae624189e66e5a9855e74d
Matsuda1129 <69665638+Matsuda1129@users.noreply.github.com>
2022-08-19 11:48:51 +0900

    product store

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -9,6 +9,12 @@ class Stock extends Model
 {
     use HasFactory;
     protected $table = 't_stocks';
+
+    protected $fillable = [
+        'product_id',
+        'type',
+        'quantity'
+    ];
     // public function product()
     // {
     //     return $this->hasMany(Product::class);
