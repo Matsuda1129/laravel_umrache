b093816dfc021284f1af74dc8e84899b8dc65e43
Matsuda1129 <69665638+Matsuda1129@users.noreply.github.com>
2022-08-22 20:51:52 +0900

    stripe before checkout

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use App\Models\User;
+use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -61,21 +62,38 @@ class CartController extends Controller
         $products = $user->products;
 
         $lineItems = [];
-        foreach ($products as $product) {
-            $lineItem = [
-                'name' => $product->name,
-                'description' => $product->information,
-                'amount' => $product->price,
-                'currency' => 'jpy',
-                'quantity' => $product->pivot->quantity,
 
-            ];
-            array_push($lineItems, $lineItem);
+        foreach ($products as $product) {
+            $quantity = '';
+            $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+
+            if ($product->pivot->quantity > $quantity) {
+                return view('user.cart.index');
+            } else {
+                $lineItem = [
+                    'name' => $product->name,
+                    'description' => $product->information,
+                    'amount' => $product->price,
+                    'currency' => 'jpy',
+                    'quantity' => $product->pivot->quantity,
+
+                ];
+                array_push($lineItems, $lineItem);
+            }
         }
         // dd($lineItems);
 
+        foreach ($products as $product) {
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1
+            ]);
+        }
+
+        dd('test');
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
-        $session = Stripe::Checkout::Session . create([
+        $session = \Stripe\Checkout::Session . create([
             'payment_method_types' => ['card'],
             'line_items' => [$lineItems],
             'mode' => 'payment',
