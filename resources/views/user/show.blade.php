663ae9f36cebf7dd994b0bf31818bda20daccee1
Matsuda1129 <69665638+Matsuda1129@users.noreply.github.com>
2022-08-22 11:35:03 +0900

    swiper

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -11,10 +11,60 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     <div class="md:flex md:justify-around">
                         <div class="md:w-1/2">
-                            <x-thumbnail filename="{{$product->imageFirst->filename  ?? ''}}" type="products" />
+                            {{--
+                            <x-thumbnail filename="{{$product->imageFirst->filename  ?? ''}}" type="products" /> --}}
+                            <!-- Slider main container -->
+                            <div class="swiper">
+                                <!-- Additional required wrapper -->
+                                <div class="swiper-wrapper">
+                                    <!-- Slides -->
+                                    <div class="swiper-slide">
+                                        @If($product->imageFirst->filename !== null)
+                                        <img src="{{ asset('storage/products/' . $product->imageFirst->filename) }}"
+                                            alt="">
+                                        @else
+                                        <img src="" alt="">
+                                        @endif
+                                    </div>
+                                    <div class="swiper-slide">
+                                        @If($product->imageSecond->filename !== null)
+                                        <img src="{{ asset('storage/products/' . $product->imageSecond->filename) }}"
+                                            alt="">
+                                        @else
+                                        <img src="" alt="">
+                                        @endif
+                                    </div>
+                                    <div class="swiper-slide">
+                                        @If($product->imageThird->filename !== null)
+                                        <img src="{{ asset('storage/products/' . $product->imageThird->filename) }}"
+                                            alt="">
+                                        @else
+                                        <img src="" alt="">
+                                        @endif
+                                    </div>
+                                    <div class="swiper-slide">
+                                        @If($product->imageFourth->filename !== null)
+                                        <img src="{{ asset('storage/products/' . $product->imageFourth->filename) }}"
+                                            alt="">
+                                        @else
+                                        <img src="" alt="">
+                                        @endif
+                                    </div>
+                                </div>
+                                <!-- If we need pagination -->
+                                <div class="swiper-pagination"></div>
+
+                                <!-- If we need navigation buttons -->
+                                <div class="swiper-button-prev"></div>
+                                <div class="swiper-button-next"></div>
+
+                                <!-- If we need scrollbar -->
+                                <div class="swiper-scrollbar"></div>
+                            </div>
                         </div>
                         <div class="md:w-1/2 ml-4">
-                            <h2 class="mb-4 text-sm title-font text-gray-500 tracking-widest">{{ $product->category->name }}
+                            <h2 class="mb-4 text-sm title-font text-gray-500 tracking-widest">{{
+                                $product->category->name }}
                             </h2>
                             <h1 class="mb-4 text-gray-900 text-3xl title-font font-medium">{{ $product->name }}</h1>
                             <p class="mb-4 leading-relaxed">{{ $product->information }}</p>
@@ -48,4 +98,7 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('js/swiper.js') }}">
+
+    </script>
 </x-app-layout>
