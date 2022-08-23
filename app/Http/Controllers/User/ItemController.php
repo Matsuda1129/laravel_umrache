1e792904ca436cfeb5f34cc9f20ac3896cf34f9b
Matsuda1129 <69665638+Matsuda1129@users.noreply.github.com>
2022-08-23 17:16:29 +0900

    testMail

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -8,6 +8,8 @@ use App\Models\Product;
 use App\Models\Stock;
 use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 class ItemController extends Controller
 {
@@ -31,12 +33,15 @@ class ItemController extends Controller
     public function index(Request $request)
     {
         // dd($request);
+        Mail::to('test@example.com')
+            ->send(new TestMail());
+
         $categories = PrimaryCategory::with('secondary')->get();
         $products = Product::availableItems()
-        ->selectCategory($request->category ?? '0')
-        ->searchKeyword($request->keyword)
-        ->sortOrder($request->sort)
-        ->paginate($request->pagination ?? '20');
+            ->selectCategory($request->category ?? '0')
+            ->searchKeyword($request->keyword)
+            ->sortOrder($request->sort)
+            ->paginate($request->pagination ?? '20');
 
         return view('user.index', compact('products', 'categories'));
     }
